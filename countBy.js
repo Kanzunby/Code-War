d3b3ebae421ce5571cd5b4a81f3c2f8d432534c3
Kanzunby <kanzunby@gmail.com>
2024-02-02 21:27:25 +0700

    add count by

--- countBy.js
+++ countBy.js
@@ -1,9 +1,12 @@
-function countBy(x, n) {
-  let z = [x];
-  for (var i = 0; i < n - 1; i++) {
-    z.push(z[i] + x);
-  }
+// function countBy(x, n) {
+//   let z = [x];
+//   for (var i = 0; i < n - 1; i++) {
+//     z.push(z[i] + x);
+//   }
 
-  return z;
-}
+//   return z;
+// }
+
+const countBy = (x, n) => [...Array(n)].map((_, i) => (i + 1) * x);
+
 console.log(countBy(2, 5));
