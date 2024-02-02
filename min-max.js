2f7fbef003eb7bfd80720c0b9418b329b94bfe87
Kanzunby <kanzunby@gmail.com>
2024-02-02 21:27:43 +0700

    add min max

---
+++ min-max.js
@@ -0,0 +1,5 @@
+const min = (list) => Math.min.apply(null, list);
+const max = (list) => Math.max.apply(null, list);
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([-52, 56, 30, 29, -54, 0, -110]));
